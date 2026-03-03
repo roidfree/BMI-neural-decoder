3d8a8158358590d403f740464f51d381b3cb5eb7
TheRealJStol <jared@stoloff.co.uk>
2026-03-03 16:38:59 +0000

    Actually passed params to the rate computing function and added causal / acausal functionality

--- positionEstimatorTrainingTesting.m
+++ positionEstimatorTrainingTesting.m
@@ -1,10 +1,12 @@
-function [modelParameters] = positionEstimatorTraining(training_data)
+function [modelParameters] = positionEstimatorTrainingTesting(training_data)
     trial_rates = struct;
     trials = size(training_data, 1);
     movements = size(training_data, 2);
+    kernel_width = 10;
+    window_width = kernel_width * 3;
     for t = 1:trials
         for m = 1:movements
-            trial_rates(t, m).rates = rates_from_spikes(training_data(t, m).spikes);
+            trial_rates(t, m).rates = rates_from_spikes(training_data(t, m).spikes, kernel_width, window_width, 1);
             trial_rates(t, m).handPos = training_data(t, m).handPos;
         end
     end
@@ -12,11 +14,13 @@ function [modelParameters] = positionEstimatorTraining(training_data)
 end
 
 
-function [rate_trains] = rates_from_spikes(spike_trains, kernel_width, window_width)
-    % Computes the estimated firing rate of all single_units using casual
-    % Gaussian filtering
+function [rate_trains] = rates_from_spikes(spike_trains, kernel_width, window_width, causal)
+    % Computes the estimated firing rate of all single_units using (casual) Gaussian filtering
+    % 'causal' = 0 for acausal and 'causal' = 1 for causal
     n_s = -window_width:window_width;
     gauss_kernel = exp(-(n_s).^2 / (2 * kernel_width.^2)) ./ (kernel_width * sqrt(2 * pi));
-    gauss_kernel(n_s > 0) = 0;  % making it causal
+    if causal == 1
+        gauss_kernel(n_s > 0) = 0;  % making it causal
+    end
     rate_trains = conv2(spike_trains, gauss_kernel, "same");
 end
